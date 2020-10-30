26c994bde28e0ac430889f84d58d21141a3783e6
juancaruizc <juancruizcruz@gmail.com>
2020-10-30 09:24:41 -0700

    completed header.js

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  const header = document.createElement('div');
+  const date = document.createElement('span');
+  const lambdaTime = document.createElement('h1');
+  const temp = document.createElement('span');
+
+  header.classList.add('header');
+  date.classList.add('date');
+  temp.classList.add('temp');
+
+  date.textContent = 'MARCH 28, 2020';
+  lambdaTime.textContent = 'Lambda Time';
+  temp.textContent = '98';
+
+  header.appendChild(date);
+  header.appendChild(lambdaTime);
+  header.appendChild(temp);
+
+  const headerContainer = document.querySelector('.header-container');
+
+  headerContainer.appendChild(header);
+
+  return header;
+}
+Header();
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,48 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+import axios from 'axios';
+
+axios
+  .get('https://lambda-times-api.herokuapp.com/topics')
+  .then((res) => {
+    const topics = res.data.topics;
+    topics.forEach((topics) => {
+      const newTopics = topicMaker(topics);
+      divTopics.appendChild(newTopics);
+    });
+  })
+  .catch((err) => {
+    return err;
+  });
+
+const title = document.querySelector('.title');
+const divTopics = document.querySelector('.topics');
+
+function topicMaker({ topics }) {
+  const topicOne = document.createElement('div');
+  const topicTwo = document.createElement('div');
+  const topicThree = document.createElement('div');
+  const topicFour = document.createElement('div');
+  const topicFive = document.createElement('div');
+
+  topicOne.classList.add('tab');
+  topicTwo.classList.add('tab');
+  topicThree.classList.add('tab');
+  topicFour.classList.add('tab');
+  topicFive.classList.add('tab');
+
+  topicOne.textContent = topics;
+  topicTwo.textContent = topics;
+  topicThree.textContent = topics;
+  topicFour.textContent = topics;
+  topicFive.textContent = topics;
+
+  topicOne.appendChild(topicTwo);
+  topicOne.appendChild(topicThree);
+  topicOne.appendChild(topicFour);
+  topicOne.appendChild(topicFive);
+
+  return topicOne;
+}
